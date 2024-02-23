a9ad43a6734497b564f6f5633291a7999b3864db
eyosHP250 <eyosila123@gmail.com>
2024-02-23 19:02:01 +0300

    add the function that gets the computer's choice, add the necessary variables

---
+++ index.js
@@ -0,0 +1,13 @@
+// input from user//
+// let input = prompt("Enter Rock,paper or scissors: ");
+// let playerChoice = input.toLowerCase();
+
+//function to get generate computer's choice//
+function getComputerChoice(){
+
+}
+//variables for getComptuerChoice//
+let x = Math.floor(Math.random()*(3));
+const options = ["rock","paper","scissors"];
+
+//function to play one Round of the game//

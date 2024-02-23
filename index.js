3c6ca11c9ad66a9acc5fdbabc3dc9d012b3cd547
eyosHP250 <eyosila123@gmail.com>
2024-02-23 19:22:48 +0300

    add playRound function that takes choice from player and computer from user and prints result

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 // input from user//
-// let input = prompt("Enter Rock,paper or scissors: ");
-// let playerChoice = input.toLowerCase();
+let input = prompt("Enter Rock,paper or scissors: ");
 
 //function to get generate computer's choice//
 function getComputerChoice(){
-
+        return options[x];
 }
 //variables for getComptuerChoice//
 let x = Math.floor(Math.random()*(3));
 const options = ["rock","paper","scissors"];
 
 //function to play one Round of the game//
+function playRound(computerChoice,playerChoice){
+    if( computerChoice === playerChoice){
+        return (`you drew ${computerChoice} and ${playerChoice} are the same`);
+    } else if ( playerChoice === 'rock' && computerChoice === 'scissors'){
+        return (`you won ${playerChoice} beat ${computerChoice}`);
+    }else if ( playerChoice === 'rock' && computerChoice === 'paper'){
+        return(`you lose ${computerChoice} beats ${playerChoice}`);
+    }else if ( playerChoice === 'paper' && computerChoice === 'rock'){
+        return (`you won ${playerChoice} beat ${computerChoice}`);
+    }else if ( playerChoice === 'paper' && computerChoice === 'scissors'){
+        return (`you lose ${computerChoice} beats ${playerChoice}`);
+    }else if ( playerChoice === 'scissors' && computerChoice === 'paper'){
+        return (`you won ${playerChoice} beat ${computerChoice}`);
+    }else if ( playerChoice === 'scissors' && computerChoice === 'rock'){
+        return (`you lose ${computerChoice} beats ${playerChoice}`);
+    }
+}
+//variables that are necessary for playROund function//
+
+let playerChoice = input.toLowerCase();
+let computerChoice = getComputerChoice();
+
+console.log(playRound(computerChoice,playerChoice));

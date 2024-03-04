5fd24c5d89bf39108565ac0db89e51901ae15c29
eyosHP250 <eyosila123@gmail.com>
2024-03-04 11:32:48 +0300

    Add the function that lets to play five round. Not functional yet

--- index.js
+++ index.js
@@ -13,34 +13,100 @@ let computerChoice = getComputerChoice();
 buttons.forEach((button) => {
   button.addEventListener("click", function () {
     buttonValues[this.id] = this.value; // Update object with button value (if buttons have IDs)
-    let val = this.value;
     const span = document.querySelector(".span");
-    span.textContent = val;
-    if(val === computerChoice){
-        span.textContent += `Draw, ${val} equals ${computerChoice}`
-    }
-    else if(val === 'rock'){
-        if(computerChoice === 'paper'){
-            span.textContent += `computer wins`
+
+    function playGame() {
+      let computerWins = 0;
+      let playerWins = 0;
+      for (let i = 1; i <= 5; i++) {
+        let input = this.value;
+        let playerChoice = input.toLowerCase();
+        let computerChoice = getComputerChoice();
+        let roundWinner = playRound(computerChoice, playerChoice);
+        if (roundWinner === "Draw") {
+          computerWins += 1;
+          playerWins += 1;
+          span.textContent = 
+            `round ${i} : is Draw : ${playerChoice} is the same as ${computerChoice}`
+          
+        } else if (roundWinner === "Player") {
+          playerWins += 1;
+          span.textContent = 
+            `round ${i} : is won by the Player: ${playerChoice} beats ${computerChoice}`
+    
+        } else if (roundWinner === "Computer") {
+          computerWins += 1;
+          span.textContent = 
+            `round ${i} : is won by the Computer: ${computerChoice} beats ${playerChoice}`
+          
         }
-        else if( computerChoice === 'scissors'){
-            span.textContent += "player wins"
-        }
-    }
-    else if(val === 'paper'){
-        if(computerChoice === 'scissors'){
-            span.textContent += `computer wins`
-        }
-        else if( computerChoice === 'rock'){
-            span.textContent += "player wins"
-        }
-    }else if(val === 'scissors'){
-        if(computerChoice === 'rock'){
-            span.textContent += `computer wins`
-        }
-        else if( computerChoice === 'paper'){
-            span.textContent += "player wins"
-        }
+      }
+      if (computerWins > playerWins) {
+        console.log(`Computer Won ${computerWins} to ${playerWins}`);
+        console.log(`Reload the page to play again`);
+      } else if (computerWins < playerWins) {
+        console.log(`Player Won ${playerWins} to ${computerWins}`);
+        console.log(`Reload the page to play again`);
+      } else {
+        console.log("Game Ends in Draw Reload the page to play again");
+      }
     }
   });
 });
+
+//function to play one Round of the game//
+
+function playRound(computerChoice, playerChoice) {
+  if (computerChoice === playerChoice) {
+    return "Draw";
+  } else if (playerChoice === "rock" && computerChoice === "scissors") {
+    return "Player";
+  } else if (playerChoice === "rock" && computerChoice === "paper") {
+    return "Computer";
+  } else if (playerChoice === "paper" && computerChoice === "rock") {
+    return "Player";
+  } else if (playerChoice === "paper" && computerChoice === "scissors") {
+    return "Computer";
+  } else if (playerChoice === "scissors" && computerChoice === "paper") {
+    return "Player";
+  } else if (playerChoice === "scissors" && computerChoice === "rock") {
+    return "Computer";
+  }
+}
+
+function playGame() {
+  let computerWins = 0;
+  let playerWins = 0;
+  for (let i = 1; i <= 5; i++) {
+    let input = this.value;
+    let playerChoice = input.toLowerCase();
+    let computerChoice = getComputerChoice();
+    let roundWinner = playRound(computerChoice, playerChoice);
+    if (roundWinner === "Draw") {
+      computerWins += 1;
+      playerWins += 1;
+      span.textContent(
+        `round ${i} : is Draw : ${playerChoice} is the same as ${computerChoice}`
+      );
+    } else if (roundWinner === "Player") {
+      playerWins += 1;
+      console.log(
+        `round ${i} : is won by the Player: ${playerChoice} beats ${computerChoice}`
+      );
+    } else if (roundWinner === "Computer") {
+      computerWins += 1;
+      console.log(
+        `round ${i} : is won by the Computer: ${computerChoice} beats ${playerChoice}`
+      );
+    }
+  }
+  if (computerWins > playerWins) {
+    console.log(`Computer Won ${computerWins} to ${playerWins}`);
+    console.log(`Reload the page to play again`);
+  } else if (computerWins < playerWins) {
+    console.log(`Player Won ${playerWins} to ${computerWins}`);
+    console.log(`Reload the page to play again`);
+  } else {
+    console.log("Game Ends in Draw Reload the page to play again");
+  }
+}
